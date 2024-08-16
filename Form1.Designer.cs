acde87e14d8ddf8efc6e3da227ee026005df9beb
TongVietHoang-1611 <hoangkahn123@gmail.com>
2024-08-16 09:42:37 +0700

    Update more matching

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -36,12 +36,23 @@
             this.processROIToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.selectROIToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.selectRegionROIToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.templateMatchingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.matchingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.multiscaleTemplateMatchingToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.multiscaleTemplateMatchingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ulityToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.transformationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.resizeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rotationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.panel1 = new System.Windows.Forms.Panel();
             this.treeView1 = new System.Windows.Forms.TreeView();
             this.panel2 = new System.Windows.Forms.Panel();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.fBMatchingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fLANNMatcherToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rotationToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1.SuspendLayout();
             this.tableLayoutPanel1.SuspendLayout();
             this.panel1.SuspendLayout();
@@ -53,7 +64,8 @@
             // 
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.fileToolStripMenuItem,
-            this.processToolStripMenuItem});
+            this.processToolStripMenuItem,
+            this.ulityToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
             this.menuStrip1.Size = new System.Drawing.Size(800, 24);
@@ -78,7 +90,9 @@
             // processToolStripMenuItem
             // 
             this.processToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.processROIToolStripMenuItem});
+            this.processROIToolStripMenuItem,
+            this.templateMatchingToolStripMenuItem,
+            this.multiscaleTemplateMatchingToolStripMenuItem});
             this.processToolStripMenuItem.Name = "processToolStripMenuItem";
             this.processToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
             this.processToolStripMenuItem.Text = "Process";
@@ -89,7 +103,7 @@
             this.selectROIToolStripMenuItem,
             this.selectRegionROIToolStripMenuItem});
             this.processROIToolStripMenuItem.Name = "processROIToolStripMenuItem";
-            this.processROIToolStripMenuItem.Size = new System.Drawing.Size(136, 22);
+            this.processROIToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.processROIToolStripMenuItem.Text = "Process ROI";
             // 
             // selectROIToolStripMenuItem
@@ -105,6 +119,72 @@
             this.selectRegionROIToolStripMenuItem.Size = new System.Drawing.Size(164, 22);
             this.selectRegionROIToolStripMenuItem.Text = "Select region ROI";
             this.selectRegionROIToolStripMenuItem.Click += new System.EventHandler(this.selectRegionROIToolStripMenuItem_Click);
+            // 
+            // templateMatchingToolStripMenuItem
+            // 
+            this.templateMatchingToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.matchingToolStripMenuItem,
+            this.multiscaleTemplateMatchingToolStripMenuItem1});
+            this.templateMatchingToolStripMenuItem.Name = "templateMatchingToolStripMenuItem";
+            this.templateMatchingToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.templateMatchingToolStripMenuItem.Text = "Template Matching";
+            this.templateMatchingToolStripMenuItem.Click += new System.EventHandler(this.templateMatchingToolStripMenuItem_Click);
+            // 
+            // matchingToolStripMenuItem
+            // 
+            this.matchingToolStripMenuItem.Name = "matchingToolStripMenuItem";
+            this.matchingToolStripMenuItem.Size = new System.Drawing.Size(238, 22);
+            this.matchingToolStripMenuItem.Text = "Matching";
+            this.matchingToolStripMenuItem.Click += new System.EventHandler(this.matchingToolStripMenuItem_Click);
+            // 
+            // multiscaleTemplateMatchingToolStripMenuItem1
+            // 
+            this.multiscaleTemplateMatchingToolStripMenuItem1.Name = "multiscaleTemplateMatchingToolStripMenuItem1";
+            this.multiscaleTemplateMatchingToolStripMenuItem1.Size = new System.Drawing.Size(238, 22);
+            this.multiscaleTemplateMatchingToolStripMenuItem1.Text = "Multi-scale Template Matching";
+            this.multiscaleTemplateMatchingToolStripMenuItem1.Click += new System.EventHandler(this.multiscaleTemplateMatchingToolStripMenuItem1_Click);
+            // 
+            // multiscaleTemplateMatchingToolStripMenuItem
+            // 
+            this.multiscaleTemplateMatchingToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fBMatchingToolStripMenuItem,
+            this.fLANNMatcherToolStripMenuItem});
+            this.multiscaleTemplateMatchingToolStripMenuItem.Name = "multiscaleTemplateMatchingToolStripMenuItem";
+            this.multiscaleTemplateMatchingToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.multiscaleTemplateMatchingToolStripMenuItem.Text = "Feature Matching";
+            this.multiscaleTemplateMatchingToolStripMenuItem.Click += new System.EventHandler(this.multiscaleTemplateMatchingToolStripMenuItem_Click);
+            // 
+            // ulityToolStripMenuItem
+            // 
+            this.ulityToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.transformationToolStripMenuItem});
+            this.ulityToolStripMenuItem.Name = "ulityToolStripMenuItem";
+            this.ulityToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.ulityToolStripMenuItem.Text = "Utility";
+            // 
+            // transformationToolStripMenuItem
+            // 
+            this.transformationToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.resizeToolStripMenuItem,
+            this.rotationToolStripMenuItem,
+            this.rotationToolStripMenuItem1});
+            this.transformationToolStripMenuItem.Name = "transformationToolStripMenuItem";
+            this.transformationToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.transformationToolStripMenuItem.Text = "Transformation";
+            // 
+            // resizeToolStripMenuItem
+            // 
+            this.resizeToolStripMenuItem.Name = "resizeToolStripMenuItem";
+            this.resizeToolStripMenuItem.Size = new System.Drawing.Size(119, 22);
+            this.resizeToolStripMenuItem.Text = "Resize";
+            this.resizeToolStripMenuItem.Click += new System.EventHandler(this.resizeToolStripMenuItem_Click);
+            // 
+            // rotationToolStripMenuItem
+            // 
+            this.rotationToolStripMenuItem.Name = "rotationToolStripMenuItem";
+            this.rotationToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.rotationToolStripMenuItem.Text = "RotationFlip";
+            this.rotationToolStripMenuItem.Click += new System.EventHandler(this.rotationToolStripMenuItem_Click);
             // 
             // contextMenuStrip1
             // 
@@ -158,13 +238,36 @@
             this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pictureBox1.Location = new System.Drawing.Point(0, 0);
             this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
             this.pictureBox1.Size = new System.Drawing.Size(658, 420);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click_1);
             this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
             this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
             this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
             this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // fBMatchingToolStripMenuItem
+            // 
+            this.fBMatchingToolStripMenuItem.Name = "fBMatchingToolStripMenuItem";
+            this.fBMatchingToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.fBMatchingToolStripMenuItem.Text = "BF Matcher";
+            this.fBMatchingToolStripMenuItem.Click += new System.EventHandler(this.fBMatchingToolStripMenuItem_Click);
+            // 
+            // fLANNMatcherToolStripMenuItem
+            // 
+            this.fLANNMatcherToolStripMenuItem.Name = "fLANNMatcherToolStripMenuItem";
+            this.fLANNMatcherToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.fLANNMatcherToolStripMenuItem.Text = "FLANN Matcher";
+            this.fLANNMatcherToolStripMenuItem.Click += new System.EventHandler(this.fLANNMatcherToolStripMenuItem_Click);
+            // 
+            // rotationToolStripMenuItem1
+            // 
+            this.rotationToolStripMenuItem1.Name = "rotationToolStripMenuItem1";
+            this.rotationToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.rotationToolStripMenuItem1.Text = "Rotation";
+            this.rotationToolStripMenuItem1.Click += new System.EventHandler(this.rotationToolStripMenuItem1_Click);
             // 
             // Form1
             // 
@@ -197,11 +300,22 @@
         private System.Windows.Forms.ToolStripMenuItem processToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem processROIToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem selectROIToolStripMenuItem;
-        private System.Windows.Forms.TreeView treeView1;
         private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem selectRegionROIToolStripMenuItem;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ToolStripMenuItem templateMatchingToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem matchingToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ulityToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem transformationToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem resizeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem rotationToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem multiscaleTemplateMatchingToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem multiscaleTemplateMatchingToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem fBMatchingToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fLANNMatcherToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem rotationToolStripMenuItem1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,16 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using Emgu.CV;
-using Emgu.Util;
 using Emgu.CV.Structure;
-using System.Collections;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Util;
+using Emgu.CV.Features2D;
+using Emgu.CV.Flann;
+using System.Diagnostics;
 
 namespace DrawingRoi
 {
@@ -20,7 +18,15 @@ namespace DrawingRoi
         Rectangle rect;
         Point StartROI, EndROI;
         bool Selecting, MouseDown;
-        
+
+        public Form1()
+        {
+            InitializeComponent();
+            Selecting = false;
+            rect = Rectangle.Empty;
+            imgList = new Dictionary<string, Image<Bgr, byte>>();
+        }
+
         private void openToolStripMenuItem_Click(object sender, EventArgs e)
         {
             try
@@ -127,6 +133,383 @@ namespace DrawingRoi
             }
         }
 
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void processToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void matchingToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (pictureBox1.Image == null || rect == null)
+                {
+                    return;
+                }
+
+                var imgScene = imgList["Input"].Clone();
+                var template = new Bitmap(pictureBox1.Image).ToImage<Bgr, byte>();
+
+                Mat imgout = new Mat();
+
+                Stopwatch stopwatch = Stopwatch.StartNew();
+                CvInvoke.MatchTemplate(imgScene, template, imgout, TemplateMatchingType.Sqdiff);
+
+
+                double minVal = 0.0;
+                double maxVal = 0.0;
+                Point minloc = new Point();
+                Point maxloc = new Point();
+
+                CvInvoke.MinMaxLoc(imgout, ref minVal, ref maxVal, ref minloc, ref maxloc);
+                Rectangle r = new Rectangle(minloc, template.Size);
+
+                CvInvoke.Rectangle(imgScene, r, new MCvScalar(255, 0, 0), 3);
+                pictureBox1.Image = imgScene.AsBitmap();
+                stopwatch.Stop();
+
+                Console.WriteLine($"Time for match template: {stopwatch.Elapsed.TotalMilliseconds} ms");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void resizeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if(!imgList.ContainsKey("ROI Image"))
+                {
+                    MessageBox.Show("Please select a template");
+                    return;
+                }
+
+                var img = new Bitmap (pictureBox1.Image).ToImage<Bgr, byte>();
+
+                img = img.Resize(1.25, Inter.Cubic);
+                pictureBox1.Image = img.AsBitmap();
+                AddImage(img, "Template resize");
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void rotationToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!imgList.ContainsKey("ROI Image"))
+                {
+                    MessageBox.Show("Please select a template");
+                    return;
+                }
+
+                var img = new Bitmap(pictureBox1.Image);
+                img.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                pictureBox1.Image = img;
+                AddImage(img.ToImage<Bgr, byte>(), "Template rotated");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+
+        private void templateMatchingToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void multiscaleTemplateMatchingToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private static VectorOfPoint ProccessImage(Image<Gray, byte> template, Image<Gray, byte> sceneImage)
+        {
+            try
+            {
+                VectorOfPoint finalPoints = new VectorOfPoint();
+                Mat homography = null;
+
+                VectorOfKeyPoint templateKeyPoints = new VectorOfKeyPoint();
+                VectorOfKeyPoint sceneKeyPoints = new VectorOfKeyPoint();
+                Mat templateDescriptor = new Mat();
+                Mat sceneDescriptor = new Mat();
+
+                Mat mask;
+                int k = 2;
+                double uniquenessthreshold = 0.8;
+                VectorOfVectorOfDMatch matches = new VectorOfVectorOfDMatch();
+
+                Brisk featureDectector = new Brisk();
+                featureDectector.DetectAndCompute(template, null, templateKeyPoints, templateDescriptor, false);
+                featureDectector.DetectAndCompute(sceneImage, null, sceneKeyPoints, sceneDescriptor, false);
+
+                BFMatcher matcher = new BFMatcher(DistanceType.Hamming);
+                matcher.Add(templateDescriptor);
+                matcher.KnnMatch(sceneDescriptor, matches, k);
+
+                mask = new Mat(matches.Size, 1, DepthType.Cv8U, 1);
+                mask.SetTo(new MCvScalar(255));
+
+                Features2DToolbox.VoteForUniqueness(matches, uniquenessthreshold, mask);
+
+                int count = Features2DToolbox.VoteForSizeAndOrientation(templateKeyPoints, sceneKeyPoints, matches, mask, 1.5, 20);
+
+                if (count >= 4)
+                {
+                    homography = Features2DToolbox.GetHomographyMatrixFromMatchedFeatures(templateKeyPoints, sceneKeyPoints, matches, mask, 5);
+                }
+
+                if (homography != null)
+                {
+                    Rectangle rect = new Rectangle(Point.Empty, template.Size);
+                    PointF[] pts = new PointF[]
+                    {
+                new PointF(rect.Left, rect.Bottom),
+                new PointF(rect.Right, rect.Bottom),
+                new PointF(rect.Right, rect.Top),
+                new PointF(rect.Left, rect.Top)
+                    };
+
+                    pts = CvInvoke.PerspectiveTransform(pts, homography);
+                    Point[] points = Array.ConvertAll<PointF, Point>(pts, Point.Round);
+                    finalPoints = new VectorOfPoint(points);
+                }
+
+                return finalPoints;
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);    
+            }
+        }
+
+        private static VectorOfPoint ProccessImageFLANN(Image<Gray, byte> template, Image<Gray, byte> sceneImage)
+        {
+            try
+            {
+                VectorOfPoint finalPoints = new VectorOfPoint();
+                Mat homography = null;
+
+                VectorOfKeyPoint templateKeyPoints = new VectorOfKeyPoint();
+                VectorOfKeyPoint sceneKeyPoints = new VectorOfKeyPoint();
+                Mat templateDescriptor = new Mat();
+                Mat sceneDescriptor = new Mat();
+
+                Mat mask;
+                int k = 2;
+                double uniquenessthreshold = 0.8;
+                VectorOfVectorOfDMatch matches = new VectorOfVectorOfDMatch();
+
+                KAZE featureDectector = new KAZE();
+                featureDectector.DetectAndCompute(template, null, templateKeyPoints, templateDescriptor, false);
+                featureDectector.DetectAndCompute(sceneImage, null, sceneKeyPoints, sceneDescriptor, false);
+
+                KdTreeIndexParams id = new KdTreeIndexParams();
+                SearchParams sp = new SearchParams();
+                FlannBasedMatcher matcher = new FlannBasedMatcher(id, sp);
+
+
+
+
+                matcher.Add(templateDescriptor);
+                matcher.KnnMatch(sceneDescriptor, matches, k);
+
+                mask = new Mat(matches.Size, 1, DepthType.Cv8U, 1);
+                mask.SetTo(new MCvScalar(255));
+
+                Features2DToolbox.VoteForUniqueness(matches, uniquenessthreshold, mask);
+
+                int count = Features2DToolbox.VoteForSizeAndOrientation(templateKeyPoints, sceneKeyPoints, matches, mask, 1.5, 20);
+
+                if (count >= 4)
+                {
+                    homography = Features2DToolbox.GetHomographyMatrixFromMatchedFeatures(templateKeyPoints, sceneKeyPoints, matches, mask, 5);
+                }
+
+                if (homography != null)
+                {
+                    Rectangle rect = new Rectangle(Point.Empty, template.Size);
+                    PointF[] pts = new PointF[]
+                    {
+                new PointF(rect.Left, rect.Bottom),
+                new PointF(rect.Right, rect.Bottom),
+                new PointF(rect.Right, rect.Top),
+                new PointF(rect.Left, rect.Top)
+                    };
+
+                    pts = CvInvoke.PerspectiveTransform(pts, homography);
+                    Point[] points = Array.ConvertAll<PointF, Point>(pts, Point.Round);
+                    finalPoints = new VectorOfPoint(points);
+                }
+
+                return finalPoints;
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+
+
+        private void fBMatchingToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (pictureBox1.Image == null || rect == null)
+
+                {
+                    return;
+                }
+
+                var imgScene = imgList["Input"].Clone();
+                var template = new Bitmap(pictureBox1.Image).ToImage<Gray, byte>();
+
+                Stopwatch stopwatch = Stopwatch.StartNew();
+                var vp = ProccessImage(template, imgScene.Convert<Gray, byte>());
+
+                if (vp != null)
+                {
+                    CvInvoke.Polylines(imgScene, vp, true, new MCvScalar(0, 0, 255), 5);
+                }
+
+                pictureBox1.Image = imgScene.AsBitmap();
+                stopwatch.Stop();
+                Console.WriteLine($"Time for BF feature matching: {stopwatch.Elapsed.TotalMilliseconds} ms");
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void fLANNMatcherToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (pictureBox1.Image == null || rect == null)
+
+                {
+                    return;
+                }
+
+                var imgScene = imgList["Input"].Clone();
+                var template = new Bitmap(pictureBox1.Image).ToImage<Gray, byte>();
+                Stopwatch stopwatch = Stopwatch.StartNew();
+                var vp = ProccessImage(template, imgScene.Convert<Gray, byte>());
+
+                if (vp != null)
+                {
+                    CvInvoke.Polylines(imgScene, vp, true, new MCvScalar(0, 0, 255), 5);
+                    stopwatch.Stop();
+                    Console.WriteLine($"Time for FLANN feature matching: {stopwatch.Elapsed.TotalMilliseconds} ms");
+                }
+
+                pictureBox1.Image = imgScene.AsBitmap();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void rotationToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!imgList.ContainsKey("ROI Image"))
+                {
+                    MessageBox.Show("Please select a template");
+                    return;
+                }
+
+                        var img = new Bitmap(pictureBox1.Image).ToImage<Bgr, byte>();
+
+                img = img.Rotate(45, new Bgr(0, 0, 0), false);
+                pictureBox1.Image = img.AsBitmap();
+                AddImage(img, "Template roatated");
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void multiscaleTemplateMatchingToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (pictureBox1.Image == null || rect == null)
+                { return; }
+                var imgScene = imgList["Input"].Clone();
+                var template = new Bitmap(pictureBox1.Image).ToImage<Bgr, byte>();
+                Rectangle r = Rectangle.Empty;
+                double GlobalminVal = float.MaxValue;
+
+                for (float scale = 0.5f; scale <= 1.5; scale += 0.25f )
+                {
+                    var temp = template.Resize(scale, Inter.Cubic);
+                    Mat imgout = new Mat();
+                    CvInvoke.MatchTemplate(imgScene, temp, imgout, TemplateMatchingType.Sqdiff);
+
+                    double minval = 0;
+                    double maxval = 0;
+                    Point minloc = new Point();
+                    Point maxloc = new Point();
+
+                    CvInvoke.MinMaxLoc(imgout, ref minval, ref maxval, ref minloc, ref maxloc);
+
+                    double prob = minval/(imgout.ToImage<Gray, byte>().GetSum().Intensity);
+
+                    if (prob < GlobalminVal)
+                    {
+                        GlobalminVal = prob;
+                        r = new Rectangle(minloc, temp.Size);
+                    }    
+                }
+
+                if (r != null)
+                {
+                    CvInvoke.Rectangle(imgScene, r, new MCvScalar(255, 0, 0), 3);
+                    pictureBox1.Image= imgScene.AsBitmap(); 
+                }    
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
         private void AddImage(Image<Bgr, byte> img, string keyname)
         {
             if (!treeView1.Nodes.ContainsKey(keyname))
@@ -146,13 +529,7 @@ namespace DrawingRoi
             }
         }
 
-        public Form1()
-        {
-            InitializeComponent();
-            Selecting = false;
-            rect = Rectangle.Empty;
-            imgList = new Dictionary<string, Image<Bgr, byte>>();
-        }
+
 
 
     }
